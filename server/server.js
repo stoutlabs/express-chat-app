ca473fa94910240210746b02ae4f9000ef2052cc
Daniel <daniel@stoutlabs.com>
2018-02-19 05:32:26 -0500

    Add socket.io basic connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "../public");
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", socket => {
+  console.log("new user connected.");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
